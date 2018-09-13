f37786683ecfb47edddd8162865841e9b23e83fa
Chad Purdy <cpurdy@flourbluffschools.net>
2018-09-13 13:05:27 -0500

    initial commit

---
+++ math_helper.py
@@ -0,0 +1,13 @@
+def my_func(a,b):
+    ''' returns a particular formula that I made up
+    '''
+    return a + b - a * b
+
+def main():
+    print(my_func(2,3))
+    
+
+if __name__ == "__main__":
+    main()
+    
+    
